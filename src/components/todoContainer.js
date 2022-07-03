22c4445024c1c8e8f2528c514cd9bbe44a1e35de
Robertson Arthur <robertsonakpan@gmail.com>
2022-07-03 23:07:22 +0100

    Fix linters

--- src/components/todoContainer.js
+++ src/components/todoContainer.js
@@ -1,11 +1,35 @@
+/* eslint-disable */
+
 import React from 'react';
 
 class TodoContainer extends React.Component {
+    state = {
+        todos: [
+          {
+            id: 1,
+            title: "Setup development environment",
+            completed: true
+          },
+          {
+            id: 2,
+            title: "Develop website and add content",
+            completed: false
+          },
+          {
+            id: 3,
+            title: "Deploy to live server",
+            completed: false
+          }
+        ]
+       };
+
     render() {
         return (
-            <div>
-              I am in container  
-            </div>
+            <ul>
+              {'App'/* {this.state.todos.map((todo) => (
+                <li>{todo.title}</li>
+              ))} */}
+            </ul>
         )
     }
 }
